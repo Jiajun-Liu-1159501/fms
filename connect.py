615d95ec3fd130e652f4b698c30a0363e24a29e8
xiaowenrou <xiaowenrou101@icloud.com>
2024-09-30 10:58:10 +1300

    update

--- connect.py
+++ connect.py
@@ -1,8 +1,8 @@
 # This is the local version: These details are available on the first MySQL Workbench screen - usually called 'Local Instance'
 # PythonAnywhere version: Copy details from Database page and change values.
 # Add this file to .gitignore so it is not synced to GitHub.
-dbuser = "root" #PUT YOUR MySQL username here
-dbpass = "**YOUR PASSWORD**" #PUT YOUR PASSWORD HERE - make sure this matches db password set in PA.
-dbhost = "localhost" 
+dbuser = "JiajunLiu1159501" #PUT YOUR MySQL username here
+dbpass = "W6jzYykFDEYeNs8" #PUT YOUR PASSWORD HERE - make sure this matches db password set in PA.
+dbhost = "JiajunLiu1159501.mysql.pythonanywhere-services.com" 
 dbport = "3306"     # Same in local and PA version
 dbname = "fms"
